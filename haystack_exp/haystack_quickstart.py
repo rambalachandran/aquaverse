159eee30d9491dc9a6b0f0625035eb129af5db80
Ram Balachandran <ramb@spiraldatagroup.com.au>
2025-05-31 07:10:50 +0930

    haystack quick start tutorial

---
+++ haystack_exp/haystack_quickstart.py
@@ -0,0 +1,45 @@
+# import os
+# %%
+from haystack import Pipeline
+from haystack.components.fetchers import LinkContentFetcher
+from haystack.components.converters import HTMLToDocument
+from haystack.components.builders import ChatPromptBuilder
+from haystack.components.generators.chat import OpenAIChatGenerator
+from haystack.dataclasses import ChatMessage
+
+# os.environ["OPENAI_API_KEY"] = "Your OpenAI API Key"
+
+fetcher = LinkContentFetcher()
+converter = HTMLToDocument()
+prompt_template = [
+    ChatMessage.from_user(
+      """
+      According to the contents of this website:
+      {% for document in documents %}
+        {{document.content}}
+      {% endfor %}
+      Answer the given question: {{query}}
+      Answer:
+      """
+    )
+]
+
+prompt_builder = ChatPromptBuilder(template=prompt_template)
+llm = OpenAIChatGenerator()
+
+pipeline = Pipeline()
+pipeline.add_component("fetcher", fetcher)
+pipeline.add_component("converter", converter)
+pipeline.add_component("prompt", prompt_builder)
+pipeline.add_component("llm", llm)
+
+pipeline.connect("fetcher.streams", "converter.sources")
+pipeline.connect("converter.documents", "prompt.documents")
+pipeline.connect("prompt.prompt", "llm")
+
+result = pipeline.run({"fetcher": {"urls": ["https://haystack.deepset.ai/overview/quick-start"]},
+              "prompt": {"query": "Which components do I need for a RAG pipeline?"}})
+
+print(result["llm"]["replies"][0].text)
+
+# %%
